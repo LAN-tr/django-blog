9628c09e34b260f68a526c8f8c1b7c0a30a8dc7f
Lucy Norman <lucynorman@mac.com>
2024-11-12 15:59:32 +0000

    removed a space

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-lantr-djangoblog-9org7ix3fg2.ws.codeinstitute-ide.net', ' .herokuapp.com']
+ALLOWED_HOSTS = ['8000-lantr-djangoblog-9org7ix3fg2.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
